5cf05f5ad40a0a061538fa1a59eafee5164e8463
2251341 <dydwksos246@naver.com>
2024-05-03 16:26:20 +0900

    ddl-auto update

--- src/main/java/com/sbs/sbb/Answer.java
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -25,11 +24,6 @@ public class Answer {
     @ManyToOne // question을 쓸 때 달아줘야 한다.
     private Question question;
 
-    // mappedBy Answer 클래스의 question 변수 이름을 적어양 함.
-    // CascadeType.REMOVE 하면 Question을 삭제 할 때 답변도 함께 삭제됨.
-    // OneToMany는 테이블의 컬럼으로 생성되지 않음.
-    // 선택
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+
 
 }
