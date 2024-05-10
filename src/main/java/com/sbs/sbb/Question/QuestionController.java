46e63720f0261f3f8c14c83ec32a7b3f5391aed1
2251341 <kimtaewoo922@gmail.com>
2024-05-10 14:35:43 +0900

    validation 유효성  체크

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -1,45 +1,48 @@
 package com.sbs.sbb.Question;
 
-import org.springframework.ui.Model;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/question") // 접두어(앞에 들어가는단어)
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
-
     private final QuestionService questionService;
 
-
     @GetMapping("/list")
-    public  String list(Model model) {
+    public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
+
         return "question_list";
-
-
     }
+
     @GetMapping("/detail/{id}")
-    public  String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id) {
         Question q = this.questionService.getQuestion(id);
 
-        model.addAttribute("question", q); //모델을 통해 보내줌
+        model.addAttribute("question", q);
+
         return "question_detail";
     }
+
     @GetMapping("/create")
     public String create() {
         return "question_form";
     }
+
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-        Question q = this.questionService.create(subject, content);
+    // QuestionForm 값을 바인딩 할 때 유효성 체크를 하라!!
+//    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+    public String questionCreate(@Valid QuestionForm questionForm){
+
+        Question q = this.questionService.create(questionForm.getSubject(), questionForm.getSubject());
 
         return "redirect:/question/list";
     }
-
-
 }
