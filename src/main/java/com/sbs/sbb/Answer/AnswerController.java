164d46db5ef0f74ae876837cdb2d38604502b7d3
2251341 <kimtaewoo922@gmail.com>
2024-05-10 16:18:06 +0900

    답변 등록에 폼 유효성 체크 적용

--- src/main/java/com/sbs/sbb/Answer/AnswerController.java
+++ src/main/java/com/sbs/sbb/Answer/AnswerController.java
@@ -3,9 +3,11 @@ package com.sbs.sbb.Answer;
 
 import com.sbs.sbb.Question.Question;
 import com.sbs.sbb.Question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content){
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult){
         Question q = this.questionService.getQuestion(id); //id 가져옴
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", q);
+            return "question_detail";
+        }
 
-        Answer answer = this.answerService.create(q,content);
+        Answer answer = this.answerService.create(q,answerForm.getContent());
 
         return "redirect:/question/detail/%s".formatted(id);
     }
