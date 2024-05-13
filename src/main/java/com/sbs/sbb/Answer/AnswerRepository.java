ab0f57cd235ee00e0bac71aa2bf3867e80c3246a
2251341 <kimtaewoo922@gmail.com>
2024-05-13 16:14:34 +0900

    @LazyCollection(LazyCollectionOption.EXTRA)를 적용하여 size()시에 SELECT COU… …NT 하도록

--- src/main/java/com/sbs/sbb/Answer/AnswerRepository.java
+++ src/main/java/com/sbs/sbb/Answer/AnswerRepository.java
@@ -1,6 +1,5 @@
 package com.sbs.sbb.Answer;
 
-import com.sbs.sbb.Answer.Answer;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
